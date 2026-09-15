ba1639067432a7c4ce535cc4ce7bc4a5112e967d
Gautam Verma <vermagautam637@gmail.com>
2026-09-15 18:56:44 +0530

    Frontend Profile Created succesfully

--- backend/jobmailer/jobmailer/urls.py
+++ backend/jobmailer/jobmailer/urls.py
@@ -5,7 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     path('api/v1/', include('mailer.api_urls')),
-    path('', include('mailer.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
